e015ef1e972184f63488fd0b79a9a4ea3d6e5ee9
neur0n <ghbaek88@gmail.com>
2026-01-27 19:18:20 +0900

    draw: step1-2 회차 조회 service 구현

--- src/modules/draw/draw.service.js
+++ src/modules/draw/draw.service.js
@@ -7,3 +7,69 @@
  * 필요 시 repository를 통해 데이터 저장 및 조회
  *
  */
+
+const repository = require('./draw.repository');
+const { formatDateTime } = require('../../common/utils');
+
+/**
+ * 추천용 목표 회차 번호 조회
+ * - 최신 회차를 목표 회차로 반환
+ * - 데이터 없으면 에러 발생
+ * @returns {Promise<number>} 목표 회차 번호
+ */
+async function getTargetDrwNo() {
+    const latestDraw = await repository.findLatestDraw();
+
+    if (!latestDraw) {
+        const error = new Error('등록된 회차 정보가 없습니다.');
+        error.status = 500;
+        throw error;
+    }
+
+    return latestDraw.drw_no;
+}
+
+/**
+ * 최신 회차 정보 조회
+ * @returns {Promise<object|null>} 회차 정보
+ */
+async function getLatestDraw() {
+    const draw = await repository.findLatestDraw();
+
+    if (!draw) {
+        return null;
+    }
+
+    return {
+        result: true,
+        drwNo: draw.drw_no,
+        drwDate: formatDateTime(draw.drw_date),
+        createdDate: formatDateTime(draw.created_date)
+    };
+}
+
+/**
+ * 특정 회차 정보 조회
+ * @param {number} drwNo - 회차 번호
+ * @returns {Promise<object|null>} 회차 정보
+ */
+async function getDrawByNo(drwNo) {
+    const draw = await repository.findDrawByNo(drwNo);
+
+    if (!draw) {
+        return null;
+    }
+
+    return {
+        result: true,
+        drwNo: draw.drw_no,
+        drwDate: formatDateTime(draw.drw_date),
+        createdDate: formatDateTime(draw.created_date)
+    };
+}
+
+module.exports = {
+    getTargetDrwNo,
+    getLatestDraw,
+    getDrawByNo,
+};
